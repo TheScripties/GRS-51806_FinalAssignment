36a95b44c4fc1dbbc5f985547313286f09b1370e
TheScripties <madeleine.vanwinkel@wur.nl>
2015-01-28 14:00:32 +0100

    Update main.R
    Added the installation of packages if needed packages are not installed.

--- main.R
+++ main.R
@@ -3,8 +3,15 @@ Created on Mon January 26 2015
 Route planner for heavy transport.
 @authors: Michiel Blok and Madeleine van Winkel
 """
+# Import packages ### instal if needed
+if (!require(base)){print("base package is not installed. Installing it now.");install.packages(base, dependencies = TRUE)} # install base package
+if (!require(downloader)){print("downloader package is not installed. Installing it now.");install.packages(downloader, dependencies = TRUE)} # install downloader package
+if (!require(rgdal)){print("rgdal package is not installed. Installing it now.");install.packages(rgdal, dependencies = TRUE)} # install rgdal package
+if (!require(raster)){print("raster package is not installed. Installing it now.");install.packages(raster, dependencies = TRUE)} # install raster package
+if (!require(sp)){print("sp package is not installed. Installing it now.");install.packages(sp, dependencies = TRUE)} # install sp package
+#if (!require(Shiny)){print("Shiny package is not installed. Installing it now.");install.packages(Shiny, dependencies = TRUE)} # install Shiny package
+#if (!require(rPython)){print("rPython package is not installed. Installing it now.");install.packages(rPython, dependencies = TRUE)} # install rPython package
 
-# Import packages
 library(base)
 library(downloader)
 library(rgdal)
