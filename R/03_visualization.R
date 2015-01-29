065cb417c5f0310a0d9d1452df26ab811b6b3e3b
TheScripties <madeleine.vanwinkel@wur.nl>
2015-01-29 14:22:50 +0100

    Update 03_visualization.R

--- R/03_visualization.R
+++ R/03_visualization.R
@@ -22,8 +22,8 @@ visualization <- function (navigatoR.coords, navigatoR.calculation) {
 
   # Plot growing arrow on map -----------------------------------------------
   for (i in 1:10) {
-    X_Temp <-  navigatoR.coords$X_Start + ((i / 10) * (navigatoR.coords$X_Dest - navigatoR.coords$X_Start))
-    Y_Temp <-  navigatoR.coords$Y_Start + ((i / 10) * (navigatoR.coords$Y_Dest - navigatoR.coords$Y_Start))
+    X_Temp <- navigatoR.coords$X_Start + ((i / 10) * (navigatoR.coords$X_Dest - navigatoR.coords$X_Start))
+    Y_Temp <- navigatoR.coords$Y_Start + ((i / 10) * (navigatoR.coords$Y_Dest - navigatoR.coords$Y_Start))
     arrows(navigatoR.coords$X_Start, navigatoR.coords$Y_Start, X_Temp, Y_Temp, col = "blue", lwd = 5)
   }
   
