d8b870ed069b7e82317cb857298555f42b3f963b
TheScripties <madeleine.vanwinkel@wur.nl>
2015-01-29 16:53:13 +0100

    Update 03_visualization.R

--- R/03_visualization.R
+++ R/03_visualization.R
@@ -29,7 +29,7 @@ visualization <- function (navigatoR.coords, navigatoR.calculation) {
   for (i in 1:10) {
     X_Temp <- navigatoR.coords$X_Start + ((i / 10) * (navigatoR.coords$X_Dest - navigatoR.coords$X_Start))
     Y_Temp <- navigatoR.coords$Y_Start + ((i / 10) * (navigatoR.coords$Y_Dest - navigatoR.coords$Y_Start))
-    arrows(navigatoR.coords$X_Start, navigatoR.coords$Y_Start, X_Temp, Y_Temp, col = "blue", lwd = 4)
+    arrows(navigatoR.coords$X_Start, navigatoR.coords$Y_Start, X_Temp, Y_Temp, col = "blue", lwd = 3)
   }
   
   # Plot KML in Google Earth ------------------------------------------------
